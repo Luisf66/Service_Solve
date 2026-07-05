1f19e1526f534e9b5f0dacf03eafcf7dbf3ab9b8
Luisf66 <Luisf311220@gmail.com>
2026-07-05 09:57:26 -0300

    feat: Update e Delete de categorias do serviço #7

--- services/urls.py
+++ services/urls.py
@@ -9,8 +9,10 @@ urlpatterns = [
     path('create/', service_views.ServiceCreateView.as_view(), name='service_create'), # Criar Serviço
     #path('<int:pk>/', ..., name='service-detail'),             # Visualizar Serviço Especifico
 
-    path('categories/', category_views.ServiceCategoryListView.as_view(), name='category_list'), # Listar Categorias
-    path('categories/create/', category_views.ServiceCategoryCreateView.as_view(), name='category_create'), # Criar Categoria
+    path('categories/', category_views.ServiceCategoryListView.as_view(), name='category_list'),                     # Listar Categorias
+    path('categories/create/', category_views.ServiceCategoryCreateView.as_view(), name='category_create'),          # Criar Categoria
+    path('categories/<int:pk>/update/', category_views.ServiceCategoryUpdateView.as_view(), name='category_update'), # Atualizar Categoria
+    path('categories/<int:pk>/delete/', category_views.ServiceCategoryDeleteView.as_view(), name='category_delete'), # Deletar Categoria
 
 #
     #path('<int:pk>/reject/', ..., name='service-reject'),      # Prestador Rejeitar Serviço
